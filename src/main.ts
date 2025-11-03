b9827854c318c3776d9adedba46607dcb903ccf0
Adrian Miguel <adriantmiguel@gmail.com>
2025-11-02 21:26:08 -0800

    saving

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,1 @@
-import { scrape } from "./scraper"; // Note the .js extension for Node ESM
-
-const url = "https://example.com";
-
-async function main() {
-  const title = await scrape(url);
-  console.log("Page title:", title);
-}
-
-main();
+console.log('hey');
