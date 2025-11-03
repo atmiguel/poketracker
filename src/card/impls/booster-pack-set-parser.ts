1dd21a652f4d7dbb4e5546783affb165e221f32e
Adrian Miguel <adriantmiguel@gmail.com>
2025-11-03 09:33:58 -0800

    starting to parse

--- src/card/impls/booster-pack-set-parser.ts
+++ src/card/impls/booster-pack-set-parser.ts
@@ -1,6 +1,20 @@
+import assert from 'assert';
 import type { Nullable } from '../../core/types/builtin';
 import type { IBoosterPackSetParser } from '../interfaces/booster-pack-set-parser';
 import type { BoosterPackSet } from '../models/booster-pack-set';
+import { load } from 'cheerio';
+
+const isTagElement = (element: cheerio.Element): element is cheerio.TagElement => {
+  return element.type === "tag";
+}
+
+const isTextElement = (element: cheerio.Element): element is cheerio.TextElement => {
+  return element.type === "text";
+}
+
+const getTagChildren = (element: cheerio.TagElement ): Array<cheerio.TagElement> => {
+  return element.children.filter(isTagElement);
+};
 
 export class BoosterPackSetParser implements IBoosterPackSetParser {
   private static instance: Nullable<BoosterPackSetParser> = null;
@@ -18,6 +32,66 @@ export class BoosterPackSetParser implements IBoosterPackSetParser {
   public readonly parseBoosterPackSets: IBoosterPackSetParser['parseBoosterPackSets'] = ({
     data,
   }): Array<BoosterPackSet> => {
-    return [];
+    const $ = load(data);
+
+    const setsTable = $('.sets-table');
+    assert(setsTable.length === 1);
+
+    const results: Array<BoosterPackSet> = [];
+    const rows = setsTable.find('tr');
+    for (const row of rows.toArray().map($)) {
+      const cells = row.children();
+
+      switch (cells.length) {
+        case 1: {
+          // series row
+          const cell = cells[0];
+          console.log($(cell).text());
+          break;
+        }
+        case 3: {
+          // either header row or pack set row
+
+          const [nameCell, releaseDateCell, cardCountCell] = cells.toArray().map($);
+          assert(nameCell !== undefined);
+          assert(releaseDateCell !== undefined);
+          assert(cardCountCell !== undefined);
+
+          if (nameCell.is('th')) {
+            // header row
+            continue;
+          }
+
+          const nameLink = nameCell.find('a');
+          assert(nameLink.length === 1);
+          const nameLinkElement = nameLink[0]!;
+          assert(isTagElement(nameLinkElement));
+
+          assert(nameLinkElement.children.length === 5);
+          const [_, __, nameElement, setIdElement, ___] = nameLinkElement.children;
+          assert(nameElement !== undefined);
+          assert(setIdElement !== undefined);
+
+          const name = $(nameElement).text().trim();
+          const setId = $(setIdElement).text().trim();
+
+          // TODO: clean this up
+          results.push({
+            id: setId,
+            name,
+            packs: [],
+            releaseDate: null,
+            series: '',
+          });
+
+          break;
+        }
+        default: {
+          throw new Error("unexpected cell count");
+        }
+      }
+    }
+
+    return results;
   };
 }
