28fd18c688dd8c112e58b92c604b3b129c153376
Adrian Miguel <adriantmiguel@gmail.com>
2025-11-02 21:35:37 -0800

    main downloads html

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,15 @@
-console.log('hey');
+import { HtmlDownloader } from "./html/impls/html-downloader";
+
+const main = async (): Promise<void> => {
+  const downloader = new HtmlDownloader();
+
+  await downloader.downloadHtml({
+    destinationFilepath: 'resources/temp.html',
+    url: 'https://pocket.limitlesstcg.com/cards',
+  });
+};
+
+main().catch((err) => {
+  console.error('Error:', err);
+  process.exit(1);
+});
