fdacd1e9f6b933ee32faee172b26ec8f2082c933
Adrian Miguel <adriantmiguel@gmail.com>
2026-01-10 13:11:52 -0800

    saving new packs

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,16 @@
 import { CCard } from './card/constants';
 import { BoosterPackSetParser } from './card/impls/booster-pack-set-parser';
 import { FileManager } from './core/file/impls/file-manager';
+import { HtmlDownloader } from './core/html/impls/html-downloader';
 
 const main = async (): Promise<void> => {
-  // await HtmlDownloader.getInstance().downloadHtml({
-  //   path: CCard.Path.BOOSTER_PACK_SETS,
-  //   url: CCard.Url.BOOSTER_PACK_SETS,
-  //   options: {
-  //     shouldOverwrite: true,
-  //   },
-  // });
+  await HtmlDownloader.getInstance().downloadHtml({
+    path: CCard.Path.BOOSTER_PACK_SETS,
+    url: CCard.Url.BOOSTER_PACK_SETS,
+    options: {
+      shouldOverwrite: true,
+    },
+  });
 
   const { contents } = await FileManager.getInstance().readFromFile({
     path: CCard.Path.BOOSTER_PACK_SETS,
